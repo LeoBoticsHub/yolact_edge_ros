44d31426324f66d5301a7eb073ed101a7ac583aa
Federico Rollo <rollo.f96@gmail.com>
2023-01-26 22:42:43 +0100

    added license

--- scripts/yolact_edge_ros.py
+++ scripts/yolact_edge_ros.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python
+'''----------------------------------------------------------------------------------------------------------------------------------
+# Copyright (C) 2022
+#
+# author: Federico Rollo
+# mail: rollo.f96@gmail.com
+#
+# Institute: Leonardo Labs (Leonardo S.p.a - Istituto Italiano di tecnologia)
+#
+# This file is part of yolact_edge_ros. <https://github.com/IASRobolab/yolact_edge_ros>
+#
+# yolact_edge_ros is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# yolact_edge_ros is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License. If not, see http://www.gnu.org/licenses/
+---------------------------------------------------------------------------------------------------------------------------------'''
 
 import sys
 
